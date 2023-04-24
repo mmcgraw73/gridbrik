9c2fea6acfbc17471e17863b2a1413edf80ac168
mmcgraw73 <shout@mcgraw.io>
2023-04-24 18:56:18 -0400

    passing in arrsObj prop from consumer

--- src/components/gridbrik.vue
+++ src/components/gridbrik.vue
@@ -1,39 +1,35 @@
 <template>
   <section>
     <div class="grid grid-rows-2 grid-flow-col gap-4 m-2 bg-slate-400">
-      <div class="bg-red-700 grid grid-cols-2" v-for="(item, i) in r1">
-        <span class="col-span-1 bg-blue-300 text-white-300">{{ r1[i] }}</span>
-        <span class="col-span-1 bg-blue-300 text-white-300">{{ r2[i] }}</span>
+      <div class="bg-red-700 grid grid-cols-2" v-for="(item, i) in arr1">
+        <span class="col-span-1 bg-blue-300 text-white-300">{{ arr1[i] }}</span>
+        <span class="col-span-1 bg-blue-300 text-white-300">{{ arr2[i] }}</span>
       </div>
-      <!-- <div class="bg-red-700">02</div>
-      <div class="bg-red-700">03</div>
-      <div class="bg-red-700">04</div>
-      <div class="bg-red-700">05</div>
-      <div class="bg-red-700">06</div>
-      <div class="bg-red-700">07</div>
-      <div class="bg-red-700">08</div>
-      <div class="bg-red-700">09</div> -->
     </div>
   </section>
 </template>
 
 <script>
-const repeater1 = [];
-const repeater2 = [
-  
-];
 export default {
   name: 'gridbrik',
   data: () => {
     return {
-      r1: repeater1,
-      r2: repeater2,
+      // r1: this.arrsObj['arr1'],
+      // r2: this.arrsObj['arr2'],
     };
   },
   props: {
     arrsObj: {
       type: Object,
       required: true,
+    },
+  },
+  computed: {
+    arr1() {
+      return this.arrsObj['arr1'];
+    },
+    arr2() {
+      return this.arrsObj['arr2'];
     },
   },
 };
