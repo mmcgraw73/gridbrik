466f66a6f04bce91411af855db8090ff646921ae
mmcgraw73 <shout@mcgraw.io>
2023-04-24 22:10:49 -0400

    minor style

--- src/components/gridbrik.vue
+++ src/components/gridbrik.vue
@@ -1,11 +1,11 @@
 <template>
   <section class="w-full">
-    <div :class="['bg-slate-400']">
-      <div class="grid grid-cols-7 gap-4" v-for="(item, i) in arr1">
-        <span class="col-span-4 bg-blue-800 text-red-300 p-3 rounded-lg mb-2">{{
+    <div :class="['bg-slate-400 p-2']">
+      <div class="grid grid-cols-7 gap-4 m-2" v-for="(item, i) in arr1">
+        <span class="col-span-3 bg-blue-800 text-red-300 p-3 rounded-lg mb-2">{{
           arr1[i]
         }}</span>
-        <span class="col-span-3 bg-blue-800 text-red-300 p-3 rounded-lg mb-2">{{
+        <span class="col-span-4 bg-blue-800 text-red-300 p-3 rounded-lg mb-2">{{
           arr2[i]
         }}</span>
       </div>
